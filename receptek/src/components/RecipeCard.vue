8580ca15065a43382b74d6f99a8a02d670df39ab
lenartaron <83168006+lenartaron@users.noreply.github.com>
2024-12-09 13:53:41 +0100

    eddig
    kurzor, káryta kezdete

---
+++ receptek/src/components/RecipeCard.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+const getImageURL = (imageURL) => {
+  return new URL(imageURL, import.meta.url).href;
+};
+
+
+
+</script>
+<template>
+<div class="card h-100">
+    <img :src="getImageURL(product.img)" alt="">
+</div>
+
+
+
+</template>
+<style scoped>
+
+
+
+
+
+</style>
