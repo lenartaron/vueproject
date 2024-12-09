f3bff12eb7116266a858fcbea85aa350f72fc6a4
lenartaron <83168006+lenartaron@users.noreply.github.com>
2024-12-09 20:27:44 +0100

    cardok
    ha bele tesszük egy v-forba akkor bele tudjunk tölteni az összes adatot és elkészíti a cardokat

--- receptek/src/components/RecipeCard.vue
+++ receptek/src/components/RecipeCard.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { recipies } from '../data/dummyData';
+
 
 const getImageURL = (imageURL) => {
   return new URL(imageURL, import.meta.url).href;
@@ -8,9 +10,16 @@ const getImageURL = (imageURL) => {
 
 </script>
 <template>
-<div class="card h-100">
-    <img :src="getImageURL(product.img)" alt="">
-</div>
+  <div class="card h-100">
+    <img :src="getImageURL(product.img)"><br>
+    <h2>{{ recipies.name }}</h2><br>
+    <p>Elkészítési idő: {{ recipies.cookTime }} perc</p>
+    <div class="diff">
+      <p>{{ recipies.difficulty }}</p>
+    </div>
+    <button type="button">Részletek</button>
+
+  </div>
 
 
 
