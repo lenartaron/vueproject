e07ff6be4fac963ddbb77fcffb66f98966eb637c
Gulyás Zsolt Máté <85568371+Tetanki-HUN@users.noreply.github.com>
2024-12-09 13:45:48 +0100

    dummyData.js és képek létrehozása
    Képek feltöltve illetve a lista  létrehozva

---
+++ receptek/src/components/data/dummyData.js
@@ -0,0 +1,30 @@
+export const recipies = [
+    {
+        id: 1,
+        name: 'Paprikás krumpli',
+        cookTime: 45,
+        difficulty: 'könnyű',
+        img: "assets/paprikaskrumpli.png"
+    },
+    {
+        id: 2,
+        name: 'Túrógombóc',
+        cookTime: 60,
+        difficulty: 'közepes',
+        img: "assets/turogomboc.png"
+    },
+    {
+        id: 3,
+        name: 'Gulyásleves',
+        cookTime: 120,
+        difficulty: 'nehéz',
+        img: "assets/gulyasleves.png"
+    },
+    {
+        id: 4,
+        name: 'Palacsinta',
+        cookTime: 30,
+        difficulty: 'könnyű',
+        img: "assets/pancake.png"
+    }
+];
