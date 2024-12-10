2a897fd0efdf884532ddf0e45c0d924633565679
Gulyás Zsolt Máté <85568371+Tetanki-HUN@users.noreply.github.com>
2024-12-10 10:38:28 +0100

    Új recept hozzáadás
    Új receptek hozzáadása sikeresen implementálva

--- receptek/src/App.vue
+++ receptek/src/App.vue
@@ -1,14 +1,26 @@
 <script setup>
 import { ref, computed } from 'vue';
 import RecipieCard from './components/RecipeCard.vue';
-import { recipies as allRecipies } from './data/dummyData';
+import NewRecipe from './components/NewRecipe.vue';
+import { recipies as dummyRecipies } from './data/dummyData';
 
+const localRecipes = ref([]);
 const searchQuery = ref('');
 const difficultyFilter = ref('');
 const sortOption = ref('cookTime');
+const currentView = ref('recipes');
+
+const addNewRecipe = (newRecipe) => {
+  localRecipes.value.push({
+    id: dummyRecipies.length + localRecipes.value.length + 1,
+    ...newRecipe,
+  });
+};
 
 const filteredRecipies = computed(() => {
-  let filtered = allRecipies.filter((recipie) =>
+  const allRecipes = [...dummyRecipies, ...localRecipes.value];
+
+  let filtered = allRecipes.filter((recipie) =>
     recipie.name.toLowerCase().includes(searchQuery.value.toLowerCase())
   );
 
@@ -26,16 +38,25 @@ const filteredRecipies = computed(() => {
 
   return filtered;
 });
+
+const switchView = (view) => {
+  currentView.value = view;
+};
 </script>
 
 <template>
   <ul>
-    <li><a href="#">Receptköny</a></li>
-    <li><a href="#">Receptek</a></li>
-    <li><a href="#">Új receptek</a></li>
+    <li><a href="#" @click.prevent="switchView('recipes')">Receptköny</a></li>
+    <li><a href="#" @click.prevent="switchView('recipes')">Receptek</a></li>
+    <li><a href="#" @click.prevent="switchView('new')">Új receptek</a></li>
     <li><a href="#">Kedvencek</a></li>
   </ul>
-      <RecipieCard :recipies="filteredRecipies" />
+  <div v-if="currentView === 'recipes'">
+    <RecipieCard :recipies="filteredRecipies" />
+  </div>
+  <div v-if="currentView === 'new'">
+    <NewRecipe @add-recipe="addNewRecipe" />
+  </div>
 </template>
 
 <style scoped>
@@ -53,5 +74,10 @@ li {
 li a {
   display: block;
   padding: 8px;
+  cursor: pointer;
+}
+
+li a:hover {
+  background-color: #ddd;
 }
 </style>
--- receptek/src/components/NewRecipe.vue
+++ receptek/src/components/NewRecipe.vue
@@ -1,20 +1,46 @@
-<template>
-    <form>
-        <input type="file" id="myFile" name="filename">
-        <input type="text">
-        <input type="number">
-        <select id="difficultyFilter">
-            <option value="">Minden nehézség</option>
-            <option value="könnyű">Könnyű</option>
-            <option value="közepes">Közepes</option>
-            <option value="nehéz">Nehéz</option>
-        </select>
-    </form>
-
-</template>
 <script setup>
+import { ref, defineEmits } from 'vue';
 
+const emit = defineEmits(['add-recipe']);
+
+const newRecipe = ref({
+  name: '',
+  cookTime: 0,
+  difficulty: '',
+  img: '',
+});
+
+const handleFileInput = (event) => {
+  const file = event.target.files[0];
+  newRecipe.value.img = URL.createObjectURL(file);
+};
+
+const submitRecipe = () => {
+  if (newRecipe.value.name && newRecipe.value.cookTime && newRecipe.value.difficulty && newRecipe.value.img) {
+    emit('add-recipe', newRecipe.value);
+    newRecipe.value = { name: '', cookTime: 0, difficulty: '', img: '' };
+    alert('Sikeresen feltöltötted ezt a receptet');
+  } else {
+    alert('Töltsed ki az összes mezőt!');
+  }
+};
 </script>
+newRecipe.cookTime
+<template>
+  <form @submit.prevent="submitRecipe">
+    <input type="file" @change="handleFileInput" />
+    <input v-model="newRecipe.name" type="text" placeholder="Recept neve" />
+    <input v-model="newRecipe.cookTime" type="number" placeholder="Főzési idő (percekben)" />
+    <select v-model="newRecipe.difficulty">
+      <option value="">Minden nehézség</option>
+      <option value="könnyű">Könnyű</option>
+      <option value="közepes">Közepes</option>
+      <option value="nehéz">Nehéz</option>
+    </select>
+    <button type="submit">Add Recipe</button>
+  </form>
+</template>
+
 <style scoped>
-    
+
 </style>
