5650d047dd045deac1baf42cfb8fc42240ae4db1
lenartaron <83168006+lenartaron@users.noreply.github.com>
2024-12-10 10:15:42 +0100

    Update RecipeCard.vue

--- receptek/src/components/RecipeCard.vue
+++ receptek/src/components/RecipeCard.vue
@@ -10,6 +10,17 @@ defineProps({
 </script>
 
 <template>
+  <input v-model="searchQuery" type="text" placeholder="Keresés receptek között..." />
+      <select v-model="difficultyFilter">
+        <option value="">Minden nehézség</option>
+        <option value="könnyű">Könnyű</option>
+        <option value="közepes">Közepes</option>
+        <option value="nehéz">Nehéz</option>
+      </select>
+      <select v-model="sortOption">
+        <option value="cookTime">Elkészítési idő</option>
+        <option value="name">Név</option>
+      </select>
 <div class="container">
   <div class="row">
     <div class="col-md-4" v-for="recipie in recipies" :key="recipie.id">
